1537adf7220e63a882a16fe65363665ae912f821
Azam Khann <49907897+azamkhan11@users.noreply.github.com>
2023-03-18 19:41:08 +0530

    Add files via upload

---
+++ src/main/java/org/simplilearn/workshop/util/StringUtil.java
@@ -0,0 +1,10 @@
+package org.simplilearn.workshop.util;
+
+public class StringUtil {
+
+	public static String fixSqlFieldValue(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
